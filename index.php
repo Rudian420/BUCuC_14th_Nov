a906f7788fd4b2ecf957df307f83cb00d34a0aaf
Rudian420 <ahmedrudian@gmail.com>
2025-12-09 02:24:18 +0600

    as

--- index.php
+++ index.php
@@ -1185,7 +1185,7 @@ $signupEnabled = getSignupStatus();
             left: -100%;
             width: 100%;
             height: 100%;
-            background: linear-gradient(90deg, transparent, rgba(255, 255, 255, 0.4), transparent);
+            background: linear-gradient(90deg, transparent, rgb(255, 255, 255), transparent);
             transition: left 0.5s;
         }
 
